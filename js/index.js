c951bdb23ad3e559c3c7024d50296177041f734e
ll <5532700+lilyn@user.noreply.gitee.com>
2020-12-26 19:46:01 +0800

    news

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // html公共部分
 $(document).ready(function () {
-    $("header").load("header.html");
-    $(".column2").load("right.html");
-    $("footer").load("footer.html");
+    $("header").load("component/header.html");
+    $(".column2").load("component/right.html");
+    $("footer").load("component/footer.html");
 })
 let imgs = document.querySelectorAll(".article .swiper-wrapper img");
 // 轮播缩略图
@@ -40,28 +40,47 @@ function renderPicNews(data) {
         str += `<li><a href="javascript:;"><img src="${el.img}" alt=""></a><h3 class="title">${el.title}</h3><div class="thumb"><i class="iconfont icon-shizhongclock77"></i><span>${el.time}</span><i class="iconfont icon-yanjing"></i><span>${el.view}</span></div></li>`
     });
     $(".picNews").html(str);
+    // 加载失败的图片显示默认图片
+    $(".picNews img").error(function () {
+        $(this).attr("src", "img/default.png")
+    })
 }
+// 获取picNews距离浏览器顶部的高度
+let newsT = $(".picNews").offset().top + 200;
 let lens = $(".page li").length - 1;
 let idx = 1;
 $(".page li").eq(idx).addClass("current").siblings().removeClass("current");
-// 获取分页器
-// 可以考虑使用事件传播
-$(".page li").click(function () {
-    if ($(this).index() == 0) {
+// 获取分页器，使用事件传播
+function pageClick(e) {
+    if ($(e.target).index() == 0) {
         if (idx > 1) {
             idx -= 1;
         } else {
             return
         }
-    } else if ($(this).index() == lens) {
+    } else if ($(e.target).index() == lens) {
         if (idx < lens - 1) {
             idx += 1;
         } else {
             return
         }
     } else {
-        idx = $(this).index();
+        idx = $(e.target).index();
     }
     getPicNews(`data/picNews${idx}.json`);
     $(".page li").eq(idx).addClass("current").siblings().removeClass("current");
-})
+    $(document).scrollTop(newsT);
+}
+$(".page").click(debounce((e) => pageClick(e), 300))
+
+function debounce(fn, wait) {
+    let timer = null,
+        result;
+    return function (...params) {
+        if (timer) clearTimeout(timer)
+        timer = setTimeout(() => {
+            result = fn.call(this, ...params);
+        }, wait)
+        return result;
+    }
+}
